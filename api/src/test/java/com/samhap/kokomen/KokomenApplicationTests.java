6d47f011830ffa6b9c65b82dc62c07af3fd28287
devhoya97 <diajbh123@gmail.com>
2025-07-15 22:35:09 +0900

    chore: 프로젝트 초기 셋팅

---
+++ api/src/test/java/com/samhap/kokomen/KokomenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhap.kokomen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokomenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ internal/src/main/java/com/samhap/kokomen/KokomenApplication.java
@@ -0,0 +1,13 @@
+package com.samhap.kokomen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokomenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokomenApplication.class, args);
+	}
+
+}
