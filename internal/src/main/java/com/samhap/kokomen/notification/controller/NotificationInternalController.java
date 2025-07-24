8bffcc1722723256f401bc85289d02a069f69bdb
와일드카고 <146502065+kargowild@users.noreply.github.com>
2025-07-24 14:32:14 +0900

    [Infra] - ci/cd, ec2, alb, mysql, logging, monitoring, nginx (#5)
    * fix: internal api에서 실수로 발생한 문제 보완
    
    * chore: ci-internal-test 추가
    
    * chore: cd 파일 추가 및 dev를 위한 docker compose를 한 곳에서 관리
    
    * chore: cd 규칙 임시 변경
    
    * chore: cd-dev에서 사용하는 도커컴포즈 파일명 수정
    
    * chore: cd-dev에서 사용하는 도커컴포즈 경로 수정
    
    * chore: cd-dev에서 사용하는 도커컴포즈 파일명 재수정
    
    * chore: cd-dev에서 발생한 문제점 보완
    
    * chore: 사용하는 도커이미지명 수정
    
    * chore: kokomen-notification-dev-internal에서 사용하는 환경변수 추가
    
    * chore: ci도 각자의 경로에 변경사항이 있을 때만 build 수행하도록 변경
    
    * chore: cd-api-dev.yml용도 미리 작성 및 cd-internal-dev.yml에서 필요없는 부분 수정
    
    * chore: boorJar 중복되는 설정 제거
    
    * style: 파일 끝에 개행 문자 추가

--- internal/src/main/java/com/samhap/kokomen/notification/controller/NotificationInternalController.java
+++ internal/src/main/java/com/samhap/kokomen/notification/controller/NotificationInternalController.java
@@ -5,6 +5,7 @@ import com.samhap.kokomen.notification.service.dto.NotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class NotificationInternalController {
     private final NotificationInternalService notificationInternalService;
 
     @PostMapping
-    public ResponseEntity<Void> saveNotification(NotificationRequest notificationRequest) {
+    public ResponseEntity<Void> saveNotification(@RequestBody NotificationRequest notificationRequest) {
         notificationInternalService.saveNotification(notificationRequest);
         return ResponseEntity.noContent().build();
     }
