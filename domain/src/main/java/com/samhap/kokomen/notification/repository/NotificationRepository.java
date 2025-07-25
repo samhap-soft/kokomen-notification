cf372cfc40f51350fcbd136a556dd9bc897720fb
nhlee98 <e5slnh9876@gmail.com>
2025-07-25 22:40:03 +0900

    feat: 멤버 id로 알림들을 조회하는 레포지토리 메소드 추가

--- domain/src/main/java/com/samhap/kokomen/notification/repository/NotificationRepository.java
+++ domain/src/main/java/com/samhap/kokomen/notification/repository/NotificationRepository.java
@@ -1,7 +1,11 @@
 package com.samhap.kokomen.notification.repository;
 
 import com.samhap.kokomen.notification.domain.Notification;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByReceiverMemberId(Long receiverMemberId, Pageable pageable);
 }
