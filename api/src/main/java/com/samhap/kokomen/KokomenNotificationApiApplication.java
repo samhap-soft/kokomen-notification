a636b720dcc6d243653f5ef233c3fbf6d6724299
nhlee98 <e5slnh9876@gmail.com>
2025-07-28 17:00:51 +0900

    style: 불필요한 개행 제거

--- api/src/main/java/com/samhap/kokomen/KokomenNotificationApiApplication.java
+++ api/src/main/java/com/samhap/kokomen/KokomenNotificationApiApplication.java
@@ -11,5 +11,4 @@ public class KokomenNotificationApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(KokomenNotificationApiApplication.class, args);
     }
-
 }
